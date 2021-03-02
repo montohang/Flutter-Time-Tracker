ed6450d8a55009c43b829199ff8499936ef1019a
montohang <harmonsitohang17@gmail.com>
2021-03-02 09:51:47 +0700

    [add sign in page layout]

---
+++ lib/ui/pages/sign_in/sign_in_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Time Tracker'),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Text('Hello'),
+      ),
+    );
+  }
+}
